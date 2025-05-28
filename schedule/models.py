240d07e778b041a45777bc7c6cb0e5efac2d3c04
Maksim <felozan4ik2007@gmail.com>
2025-05-28 20:30:54 +0300

    new classes

--- schedule/models.py
+++ schedule/models.py
@@ -30,3 +30,37 @@ class Student(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class Schedule(models.Model):
+    DAYS_CHOICES = [
+        ('Monday', 'Monday'),
+        ('Tuesday', 'Tuesday'),
+        ('Wednesday', 'Wednesday'),
+        ('Thursday', 'Thursday'),
+        ('Friday', 'Friday'),
+    ]
+    LESSON_NUMBERS_CHOICES = [
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+        (6, '6'),
+    ]
+
+    day = models.CharField(max_length=10, choices=DAYS_CHOICES)
+    lesson_number = models.IntegerField(choices=LESSON_NUMBERS_CHOICES)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name='lessons')
+    lesson_class = models.ForeignKey(Class, on_delete=models.CASCADE, related_name='schedules')
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, related_name='lessons')
+
+
+class Grade(models.Model):
+    grade = models.IntegerField()
+    date = models.DateField()
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='grades')
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name='grades')
+
+    def __str__(self):
+        return f"{self.student.name} - {self.subject.name}: {self.grade}"    
